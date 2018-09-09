6993cb0e709aee58184f7e7dc1432e0ad7ac9ada
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 13:00:47 +0200

    First commit

---
+++ src/main/java/methodlength/WriteShortUnitsOfCode.java
@@ -0,0 +1,58 @@
+package methodlength;
+
+/**
+ * Does the analyzer count lines or statements?
+ * Does it include comments?
+ */
+public class WriteShortUnitsOfCode {
+
+	public void statements16Comments0Lines16() {
+		int value = 0;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+		value = value +1;
+	}
+
+	public void statements16Comments0Lines8() {
+		int value = 0; value = value +1;
+		value = value +1; value = value +1;
+		value = value +1; value = value +1;
+		value = value +1; value = value +1;
+		value = value +1; value = value +1;
+		value = value +1; value = value +1;
+		value = value +1; value = value +1; 
+		value = value +1; value = value +1;
+	}
+	
+	public void statements2Comments14Lines16() {
+		int value = 0;
+		// A comment 1
+		// A comment 2
+		// A comment 3
+		// A comment 4
+		// A comment 5
+		// A comment 6
+		// A comment 7
+		// A comment 8
+		// A comment 9
+		// A comment 10
+		// A comment 11
+		// A comment 12
+		// A comment 13
+		// A comment 14
+		value = value +1;
+	}
+
+}
