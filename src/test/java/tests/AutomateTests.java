3c92629f8749cf1bcf9714a70c406a804ef6b574
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 14:36:52 +0200

    automate tests

--- src/test/java/tests/AutomateTests.java
+++ src/test/java/tests/AutomateTests.java
@@ -4,10 +4,106 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import codeduplication.WriteCodeOnce;
+import complexity.WriteSimpleUnitsOfCode;
+import complexity.WriteSimpleUnitsOfCodeWithObjects;
+import methodargs.KeepUnitInterfacesSmall;
+import methodlength.WriteShortUnitsOfCode;
+
 public class AutomateTests {
 
 	@Test
-	public void shouldTest() {
+	public void shouldAcceptPassingTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void shouldAcceptFailingTest() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void shouldRefenceClasses() {
+		WriteCodeOnce.class.getName();
+		WriteSimpleUnitsOfCode.class.getName();
+		WriteSimpleUnitsOfCodeWithObjects.class.getName();
+		KeepUnitInterfacesSmall.class.getName();
+		WriteShortUnitsOfCode.class.getName();
+	}
+	
+	@Test
+	public void shouldHaveLotsOfLinesWithAsserts() {
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
 		assertTrue(true);
 		assertTrue(true);
 		assertTrue(true);
