f9114d6137a9d8cd5f3caddeb43a81262f355dad
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 14:54:17 +0200

    automate tests

--- src/test/java/tests/AutomateTests.java
+++ src/test/java/tests/AutomateTests.java
@@ -10,6 +10,20 @@ import complexity.WriteSimpleUnitsOfCodeWithObjects;
 import methodargs.KeepUnitInterfacesSmall;
 import methodlength.WriteShortUnitsOfCode;
 
+/**
+ * Test code metrics: 
+ * - The number of lines of test code should be at least 50% of the production code.
+ * - The number of lines of test code with an assert should be at least 5%.
+ * 
+ * Untested Files
+ * - Classes are removed from this list when sufficient lines are covered by method invocations to the class.
+ * 
+ * Interesting:
+ * - Tests don't have to pass.
+ * - Tests don't have to test anything.
+ * - Test code can be plain wrong.
+ * - Tests are not checked for duplication.
+ */
 public class AutomateTests {
 
 	@Test
@@ -27,6 +41,7 @@ public class AutomateTests {
 	 */
 	@Test
 	public void shouldRefenceClasses() {
+		someHelperMethd(1, 21, 3, 4, 5, 6);
 		WriteCodeOnce.class.getName();
 		WriteSimpleUnitsOfCode.class.getName();
 		WriteSimpleUnitsOfCodeWithObjects.class.getName();
@@ -34,27 +49,71 @@ public class AutomateTests {
 		WriteShortUnitsOfCode.class.getName();
 	}
 	
+	/**
+	 * Note that some classes required more than one invocation for it to be removed from the list.
+	 */
 	@Test
 	public void shouldInvokeMethods() {
 		WriteCodeOnce once = null;
 		once.anotherCopyWithEmptyLines();
+		
 		WriteSimpleUnitsOfCode simple = null;
 		simple.fiveWithFor();
+		
 		WriteSimpleUnitsOfCodeWithObjects obj = null;
 		obj.fiveWithOptionalCondition(0);
 		obj.fiveWithStreamingApi();
 		obj.fiveWithStreamingApiTwo();
+		
 		KeepUnitInterfacesSmall small = null;
 		small.fourArg(1, 2, 3, 4);
+		
 		WriteShortUnitsOfCode shrt = null;
 		shrt.longStatementOverMultipleLines(1L);
 		shrt.longStatementOverOneLine(1L);
 		shrt.statements16Comments0Lines16();
 	}
 	
+	public int someHelperMethd(int a,int b,int c,int d,int e,int f) {
+		return a+b+c+d+e+f;
+	}
 	
 	@Test
 	public void shouldHaveLotsOfLinesWithAsserts() {
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
 		assertTrue(true);
 		assertTrue(true);
 		assertTrue(true);
