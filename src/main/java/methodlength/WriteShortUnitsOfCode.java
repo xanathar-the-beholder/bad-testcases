bb14237a81bfc7ad14701c5918dc2bdaab65cc48
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 13:06:25 +0200

    More methodlength test cases.

--- src/main/java/methodlength/WriteShortUnitsOfCode.java
+++ src/main/java/methodlength/WriteShortUnitsOfCode.java
@@ -1,27 +1,27 @@
 package methodlength;
 
 /**
- * Does the analyzer count lines or statements?
- * Does it include comments?
+ * Does the analyzer count lines or statements? -> Lines
+ * Does it include comments? -> No
  */
 public class WriteShortUnitsOfCode {
 
 	public void statements16Comments0Lines16() {
 		int value = 0;
 		value = value +1;
+		value = value +2;
 		value = value +1;
+		value = value +3;
 		value = value +1;
+		value = value +2;
 		value = value +1;
+		value = value +4;
 		value = value +1;
+		value = value +2;
 		value = value +1;
+		value = value +5;
 		value = value +1;
-		value = value +1;
-		value = value +1;
-		value = value +1;
-		value = value +1;
-		value = value +1;
-		value = value +1;
-		value = value +1;
+		value = value +2;
 		value = value +1;
 	}
 
@@ -52,6 +52,44 @@ public class WriteShortUnitsOfCode {
 		// A comment 12
 		// A comment 13
 		// A comment 14
+		value = value +1;
+	}
+	
+	public void statements2MultilineComments14Lines16() {
+		int value = 0;
+		/* A comment 1
+		   A comment 2
+		   A comment 3
+		   A comment 4
+		   A comment 5
+		   A comment 6
+		   A comment 7
+		   A comment 8
+		   A comment 9
+		   A comment 10
+		   A comment 11
+		   A comment 12
+		   A comment 13
+		   A comment 14 */
+		value = value +1;
+	}
+	
+	public void statements2JavadocComments14Lines16() {
+		int value = 0;
+		/** A comment 1
+		   A comment 2
+		   A comment 3
+		   A comment 4
+		   A comment 5
+		   A comment 6
+		   A comment 7
+		   A comment 8
+		   A comment 9
+		   A comment 10
+		   A comment 11
+		   A comment 12
+		   A comment 13
+		   A comment 14 */
 		value = value +1;
 	}
 
