272be7922a04699318555e6c50eacf7e815315e5
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 14:13:43 +0200

    SimpleUnits of Code

--- src/main/java/complexity/WriteSimpleUnitsOfCode.java
+++ src/main/java/complexity/WriteSimpleUnitsOfCode.java
@@ -2,6 +2,7 @@ package complexity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class WriteSimpleUnitsOfCode {
@@ -79,6 +80,21 @@ public class WriteSimpleUnitsOfCode {
 			.filter(i -> i > 3)
 			.filter(i -> i > 4)
 			.collect(Collectors.toList());
+	}
+
+	public void fiveWithStreamingApiTwo() {
+		List<Integer> list = new ArrayList<>();
+		list.stream()
+			.filter(i -> i > 0 || i >1 || i > 2 || i>3 || i>4)
+			.collect(Collectors.toList());
+	}
+
+	public void fiveWithOptionalCondition(Integer value) {
+		value = value + Optional.ofNullable(value).orElse(2);
+		value = value + Optional.ofNullable(value).orElse(3);
+		value = value + Optional.ofNullable(value).orElse(4);
+		value = value + Optional.ofNullable(value).orElse(5);
+		value = value + Optional.ofNullable(value).orElse(6);
 	}
 
 }
