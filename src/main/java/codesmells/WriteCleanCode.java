d13e8181d9619c21ee09123f9ff449f53f39d4b1
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-09 15:18:25 +0200

    Clean Code

--- src/main/java/codesmells/WriteCleanCode.java
+++ src/main/java/codesmells/WriteCleanCode.java
@@ -6,21 +6,38 @@ import java.io.IOException;
 
 public class WriteCleanCode {
 
+	/**
+	 * Todo is flagged.
+	 */
 	public void leaveNoBadCommentsBehind() {
 		// TODO: Make this method a lot faster some day
 		// CHECKSTYLE:OFF
 		// JIRA-1234: Fixes a bug when summing negative numbers
 	}
 	
+	/**
+	 * Not flagged
+	 */
 	public void leaveNoCodeInCommentsBehind_one() {
 		// System.out.println("Test")
 	}
 
+	/**
+	 * Not flagged
+	 */
 	public void leaveNoCodeInCommentsBehind_two() {
 		// WriteCleanCode.class.getName()
 		// new WriteCodeOnce().anotherCopyWithEmptyLines();
 	}
 
+	public void leaveNoCodeInCommentsBehind_three() {
+		// leaveNoDeadCodeBehind_deadBranch();
+		// leaveNoLongIdentifiersBehindWellThisIdentifierIsRatherLongAndContainsStringIntegerBooleanAndDouble();
+	}
+
+	/**
+	 * Not flagged
+	 */
 	public int leaveNoDeadCodeBehind_deadBranch() {
 		if (true) {
 			return 42;
@@ -29,18 +46,30 @@ public class WriteCleanCode {
 		}
 	}
 	
+	/**
+	 * Not flagged
+	 */
 	private void leaveNoDeadCodeBehind_unusedPrivateMethod() {
 		System.out.println("Hi!");
 	}
 	
+	/**
+	 * Not flagged
+	 */
 	public boolean leaveNoLongIdentifiersBehindWellThisIdentifierIsRatherLongAndContainsStringIntegerBooleanAndDouble() {
 		return true;
 	}
 	
+	/**
+	 * Not flagged
+	 */
 	public int leaveNoMagicConstantsBehind(int value) {
 		return (value * 42 + 72 - 3) / 4;
 	}
 	
+	/**
+	 * Flagged
+	 */
 	public void leaveNoBadlyHandledExceptionBehind_Swallow() {
 		try {
 			new FileInputStream(new File("pom.xml"));
@@ -49,6 +78,9 @@ public class WriteCleanCode {
 		}
 	}
 	
+	/**
+	 * Not flagged
+	 */
 	public void leaveNoBadlyHandledExceptionBehind_Throwable() {
 		try {
 			leaveNoDeadCodeBehind_deadBranch();
@@ -56,8 +88,5 @@ public class WriteCleanCode {
 			throw new RuntimeException(ex);
 		}
 	}
-	
-	
-	
 	
 }
